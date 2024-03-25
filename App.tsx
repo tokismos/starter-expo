c3ff3c57b0bfae082b5edad430ea34ca989e5488
Berrada, Mohammed <Mohammed.Berrada@hmhco.com>
2024-03-25 02:50:23 -0400

    test

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
+      <Text>Open up App.tsx to start working on yoddurs app!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
